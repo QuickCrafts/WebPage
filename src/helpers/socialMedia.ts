544f6b62230b1ff214c09363731effb63777e6e1
dgosunar <dgosunar@unal.edu.co>
2023-12-27 15:53:14 -0500

    sobre nosotros listo

---
+++ src/helpers/socialMedia.ts
@@ -0,0 +1,26 @@
+import type { NameMedia } from "../interfaces/teamMember.astro";
+
+export const getImageMedia = (media: NameMedia, style: 'dark' | 'light'): string => {
+    const route = '/media/' + style;
+
+    switch (media) {
+        case 'FACEBOOK':
+            return `${route}/facebook.svg`;
+        case 'GITHUB':
+            return `${route}/github.svg`;
+        case 'INSTAGRAM':
+            return `${route}/instagram.svg`;
+        case 'LINKEDIN':
+            return `${route}/linkedin.svg`;
+        case 'WEB':
+            return `${route}/web.svg`;
+        case 'GOOGLE':
+            return `${route}/google.svg`;
+        case 'TIKTOK':
+            return `${route}/tiktok.svg`;
+        case 'TWITTER':
+            return `${route}/twitter.svg`;
+        default:
+            return '';
+    }
+}
